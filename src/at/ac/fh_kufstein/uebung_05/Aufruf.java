a5d9feeb06325f9d8a9eea30a44dd40316309ce3
manuel-seisl <1810653153@stud.fh-kufstein.ac.at>
2018-12-13 12:32:27 +0100

    Leistungsbeurteilung - Aufgabe 4

--- src/at/ac/fh_kufstein/uebung_05/Aufruf.java
+++ src/at/ac/fh_kufstein/uebung_05/Aufruf.java
@@ -3,30 +3,56 @@ package at.ac.fh_kufstein.uebung_05;
 import at.ac.fh_kufstein.uebung_05.classes.Auto;
 import at.ac.fh_kufstein.uebung_05.classes.Fahrzeug;
 import at.ac.fh_kufstein.uebung_05.classes.Wasserfahrzeug;
+import com.sun.codemodel.internal.JOp;
 
-import java.util.ArrayList;
-import java.util.List;
+import javax.swing.*;
+import java.lang.reflect.Array;
+import java.util.*;
 
 public class Aufruf
 {
 
     public static void main(String[] args){
 
-        List<Fahrzeug> fahrzeugList = new ArrayList<>();
+        /*List<Auto> fahrzeugList = new ArrayList<>();
 
-        Auto bmw = new Auto((short)4, "silber", (short)220, (short)5, false, (short)0, false, (short)4);
-        Auto audi = new Auto((short)4, "schwarz", (short)180, (short)5, false, (short)0, false, (short)8);
+        fahrzeugList.add(new Auto((short)4, "silber", (short)100, (short)5, false, (short)0, false, (short)4));
+        fahrzeugList.add(new Auto((short)4, "schwarz", (short)180, (short)5, false, (short)0, false, (short)8));
+        fahrzeugList.add(new Auto((short)4, "blau", (short)150, (short)5, false, (short)0, false, (short)4));
+        fahrzeugList.add(new Auto((short)4, "rot", (short)220, (short)5, false, (short)0, false, (short)4));
+        fahrzeugList.add(new Auto((short)4, "grün", (short)300, (short)5, false, (short)0, false, (short)4));
 
-        Wasserfahrzeug titanic = new Wasserfahrzeug((short)0, "blau", (short)51000, (short)0, false, (short)0, 10.54, (short)3, 100.000);
-        titanic.setGeschwindigkeit((short)-14536);
+        Collections.sort(fahrzeugList);
 
-        fahrzeugList.add(bmw);
-        fahrzeugList.add(audi);
-        fahrzeugList.add(titanic);
+        for ( Auto a : fahrzeugList ){
+            System.out.println(a.toString());
+        };*/
 
-        for (Fahrzeug item : fahrzeugList){
-            System.out.println(item.toString());
+        // --- Aufgabe 4 ---
+
+        String var1 = JOptionPane.showInputDialog("Bitte 1.Eingabe tätigen:");
+        String var2 = JOptionPane.showInputDialog("Bitte 2.Eingabe tätigen:");
+        String var3 = JOptionPane.showInputDialog("Bitte 3.Eingabe tätigen:");
+
+        List<String> inputs = new ArrayList<>();
+        List<String> list = new ArrayList<>();
+
+        inputs.add(var1);
+        inputs.add(var2);
+        inputs.add(var3);
+
+        for (String item : inputs){
+
+            if (list.contains(item)){
+                System.out.println("Es wurde ein gleicher Text eingegeben!");
+                return;
+            }
+
+            list.add(item);
+
         }
+
+        System.out.println("Es wurde kein gleicher Text eingegeben!");
 
     }
 
