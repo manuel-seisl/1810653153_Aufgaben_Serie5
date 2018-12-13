086e6790e083c581839b33917fa854d688f8d40f
manuel-seisl <1810653153@stud.fh-kufstein.ac.at>
2018-12-13 10:50:05 +0100

    Aufgabe 7

--- src/at/ac/fh_kufstein/uebung_05/Aufruf.java
+++ src/at/ac/fh_kufstein/uebung_05/Aufruf.java
@@ -1,12 +1,18 @@
 package at.ac.fh_kufstein.uebung_05;
 
 import at.ac.fh_kufstein.uebung_05.classes.Auto;
+import at.ac.fh_kufstein.uebung_05.classes.Fahrzeug;
 import at.ac.fh_kufstein.uebung_05.classes.Wasserfahrzeug;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Aufruf
 {
 
     public static void main(String[] args){
+
+        List<Fahrzeug> fahrzeugList = new ArrayList<>();
 
         Auto bmw = new Auto((short)4, "silber", (short)220, (short)5, false, (short)0, false, (short)4);
         Auto audi = new Auto((short)4, "schwarz", (short)180, (short)5, false, (short)0, false, (short)8);
@@ -14,9 +20,13 @@ public class Aufruf
         Wasserfahrzeug titanic = new Wasserfahrzeug((short)0, "blau", (short)51000, (short)0, false, (short)0, 10.54, (short)3, 100.000);
         titanic.setGeschwindigkeit((short)-14536);
 
-        System.out.println(bmw.toString());
-        System.out.println(audi.toString());
-        System.out.println(titanic.toString());
+        fahrzeugList.add(bmw);
+        fahrzeugList.add(audi);
+        fahrzeugList.add(titanic);
+
+        for (Fahrzeug item : fahrzeugList){
+            System.out.println(item.toString());
+        }
 
     }
 
