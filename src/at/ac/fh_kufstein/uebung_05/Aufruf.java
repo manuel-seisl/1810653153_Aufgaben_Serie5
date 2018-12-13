b71504fe4c3285159bb39f07e7634683e7afb96d
manuel-seisl <1810653153@stud.fh-kufstein.ac.at>
2018-12-13 10:42:13 +0100

    Aufgabe 5

--- src/at/ac/fh_kufstein/uebung_05/Aufruf.java
+++ src/at/ac/fh_kufstein/uebung_05/Aufruf.java
@@ -1,5 +1,6 @@
 package at.ac.fh_kufstein.uebung_05;
 
+import at.ac.fh_kufstein.uebung_05.classes.Auto;
 import at.ac.fh_kufstein.uebung_05.classes.Wasserfahrzeug;
 
 public class Aufruf
@@ -7,7 +8,9 @@ public class Aufruf
 
     public static void main(String[] args){
 
-
+        Auto bmw = new Auto((short)4, "silber", (short)220, (short)5, false, (short)0, false, (short)4);
+        Auto audi = new Auto((short)4, "schwarz", (short)180, (short)5, false, (short)0, false, (short)8);
+        Wasserfahrzeug titanic = new Wasserfahrzeug((short)0, "blau", (short)51000, (short)0, false, (short)0, 10.54, (short)3, 100.000);
 
     }
 
